cfe3d57fe5cdfdf6ace0aafca885437ceb0ba35e
goatx-dev <86360571+goatx-dev@users.noreply.github.com>
2021-09-29 08:36:19 -0500

    Initial Push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
+import { AssertNotNull } from '@angular/compiler';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AssetListComponent } from './asset-list/asset-list.component';
+import { DataResolver } from './data.resolver';
 import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomePageComponent
+    component: HomePageComponent,
+    resolve: { data: DataResolver }
+  },
+  {
+    path: 'assets/:id',
+    component: AssetListComponent,
+    resolve: { data: DataResolver }
   }
 ];
 
