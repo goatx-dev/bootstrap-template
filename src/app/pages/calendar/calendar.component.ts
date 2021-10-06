c98c8eb708cbfa852d78cf1ab4aa41ed1fc634de
Chef Joe <Mestopholis77@gmail.com>
2021-10-06 16:46:33 -0500

    calendar

--- src/app/pages/calendar/calendar.component.ts
+++ src/app/pages/calendar/calendar.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FullCalendarModule } from '@fullcalendar/angular'; // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
 import interactionPlugin from '@fullcalendar/interaction'; // a plugin!
-import { CalendarOptions } from '@fullcalendar/angular';
-
+import { CalendarOptions } from '@fullcalendar/angular'; // useful for typechecking
 
 @Component({
   selector: 'app-calendar',
@@ -14,19 +13,32 @@ export class CalendarComponent implements OnInit {
 
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
-    dateClick: this.handleDateClick.bind(this), // bind is important!
+    dateClick: this.handleDateClick.bind(this), 
+    eventClick: this.handleEventClick.bind(this),
+    themeSystem: 'bootstrap',
     events: [
-      { title: 'Birthday: Jeff', date: '2021-10-09' },
-      { title: 'Azra', date: '2021-10-15' }
-    ]
+      { title: 'Red Event', date: '2021-10-01', color: '#ff0000', textColor: '#FFFFFF'},
+      { title: 'Green Event', date: '2021-10-02', color: '#00ff00', textColor: '#000000'}
+    ],
+    editable: true,
+    droppable: true,
   };
 
   handleDateClick(arg: any) {
-    alert('date click! ' + arg.dateStr)
+   // do work here on the event
+   console.log(arg)
+  }
+
+  handleEventClick(arg: any)
+  {
+    console.log(arg)
   }
   constructor() { }
 
-  ngOnInit(): void {
+
+  ngOnInit() {
+   
+    
   }
 
 }
